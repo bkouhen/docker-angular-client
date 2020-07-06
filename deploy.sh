77f9e25dfc40d143a9b0997e40e735b80bd3501c
EL KOUHENE Boubker <boubker.elkouhene2@soprasteria.com>
2020-07-06 10:48:54 +0200

    Travis deploy only to ECS AWS 4

--- deploy.sh
+++ deploy.sh
@@ -15,12 +15,5 @@ sudo chmod +x /usr/bin/ecs-deploy
 # login AWS ECR
 # eval $(aws ecr get-login --region eu-west-3)
 
-# or login DockerHub
-echo "$DOCKER_PWD" | docker login -u "$DOCKER_ID" --password-stdin
-
-# build the docker image and push to an image repository
-docker build -t bkouhen/docker-angular-client .
-docker push bkouhen/docker-angular-client:latest
-
 # update an AWS ECS service with the new image
-ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -r eu-west-3 -i bkouhen/docker-angular-client:latest
+ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i bkouhen/docker-angular-client:latest
